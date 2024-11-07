a8bf8c02076e7c6d751348e66ffbcb796d3f6924
bemojo <morgan.jonasson@gmail.com>
2024-11-07 09:04:55 +0100

    Fix validation issues: - Update manifest.json format - Add config schema - Update GitHub workflow to use node20 - Update HACS configuration

--- custom_components/glue_lock/__init__.py
+++ custom_components/glue_lock/__init__.py
@@ -2,25 +2,21 @@
 from __future__ import annotations
 
 import logging
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
 
 from .const import DOMAIN, PLATFORMS
 
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup(hass: HomeAssistant, config: dict) -> bool:
-    """Set up the Glue Lock component."""
-    hass.data.setdefault(DOMAIN, {})
-    return True
+# Indicate that we only support config flow
+CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Glue Lock from a config entry."""
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = entry.data
-
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
@@ -28,5 +24,5 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
+        hass.data.pop(DOMAIN, None)
     return unload_ok
